455aed9e7a7d0882cf71d7aa8f50f8fee83a94b8
R Vandanaa <vandanaakrishna2006@gmail.com>
2025-09-06 12:15:23 +0530

    version5

--- ecofinds/ecofinds/urls.py
+++ ecofinds/ecofinds/urls.py
@@ -1,22 +1,28 @@
-"""
-URL configuration for ecofinds project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # auth
+    path("signup/", views.signup, name="signup"),
+    path("login/",  auth_views.LoginView.as_view(template_name="marketplace/login.html"), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+
+    # user
+    path("dashboard/", views.dashboard, name="dashboard"),
+
+    # products
+    path("", views.product_list, name="product_list"),
+    path("products/new/", views.product_create, name="product_create"),
+    path("products/mine/", views.my_listings, name="my_listings"),
+    path("products/<int:pk>/", views.product_detail, name="product_detail"),
+    path("products/<int:pk>/edit/", views.product_update, name="product_update"),
+    path("products/<int:pk>/delete/", views.product_delete, name="product_delete"),
+
+    # cart & purchases
+    path("cart/", views.cart_view, name="cart"),
+    path("cart/add/<int:pk>/", views.add_to_cart, name="add_to_cart"),
+    path("cart/remove/<int:item_id>/", views.remove_from_cart, name="remove_from_cart"),
+    path("checkout/", views.checkout, name="checkout"),
+    path("purchases/", views.purchases, name="purchases"),
 ]
